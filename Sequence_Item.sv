e941afa7c6039d183228370b946fdad0f71b371f
eman zakria <82594179+emanazakria@users.noreply.github.com>
2023-02-22 22:46:46 +0200

    Update Sequence_Item.sv

--- Sequence_Item.sv
+++ Sequence_Item.sv
@@ -13,18 +13,18 @@ class Sequence_Item extends uvm_sequence_item;
 	//class variables 
 	bit 							clk 		;
 	logic 							rst 		;
-	logic 							en 			;
-	logic							wr 			;
+	logic 							en 		;
+	logic							wr 		;
 	
 	logic 							Valid_Out 	;
-	logic 		[Data_Width-1:0] 	Data_Out 	;
+	logic 		[Data_Width-1:0] 			Data_Out 	;
 	
 	//random variables 
-	randc bit 	[Data_Width-1:0] 	Data_In		;
-	randc bit 	[Add_Width-1:0] 	Addr 		;
+	randc bit 	[Data_Width-1:0] 			Data_In		;
+	randc bit 	[Add_Width-1:0] 			Addr 		;
 
 	//constraints for random variables
-	constraint c1 	{Addr 		inside{[0:31]};};
-	constraint c2 	{Data_In 	inside{[0:31]};};
+	constraint c1 	{Addr 		inside{[0:31]};}			;
+	constraint c2 	{Data_In 	inside{[0:31]};}			;
 	
 endclass
