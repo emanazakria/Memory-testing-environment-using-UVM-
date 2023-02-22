2f5fdec262a7f3523eadb24932768ea52ba9ed38
eman zakria <82594179+emanazakria@users.noreply.github.com>
2023-02-22 22:51:40 +0200

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,14 +1,14 @@
 interface intf #(parameter Add_Width = 32 , Data_Width = 32);
 
-	bit 								clk 		;
-	logic 								rst 		;
-	logic 								en 			;
-	logic								wr 			;
+	bit 						clk 		;
+	logic 						rst 		;
+	logic 						en 		;
+	logic						wr 		;
 	
 	bit 		[Data_Width-1:0] 		Data_In		;
 	bit 		[Add_Width-1:0] 		Addr 		;
 	
-	logic 								Valid_Out 	;
+	logic 						Valid_Out 	;
 	logic 		[Data_Width-1:0] 		Data_Out 	;
 	
 endinterface
