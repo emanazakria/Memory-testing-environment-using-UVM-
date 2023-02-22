9e1dd5a980816fd12a31d97be7e87a4e849bd79c
eman zakria <82594179+emanazakria@users.noreply.github.com>
2023-02-22 22:37:23 +0200

    Update Agent.sv

--- Agent.sv
+++ Agent.sv
@@ -4,9 +4,9 @@ class Agent extends uvm_agent;
 	Driver			Driver1  	;
 	Monitor			Monitor1 	;
 	Sequencer		Sequencer1 	;
-	virtual intf	Confg_intf	;
-	virtual intf	Local_intf 	;
-    uvm_analysis_port #(Sequence_Item) my_analysis_port;
+	virtual intf		Confg_intf	;
+	virtual intf		Local_intf 	;
+    	uvm_analysis_port #(Sequence_Item) my_analysis_port;
 	
 	function new (string Name = "Agent" , uvm_component parent = null);
 		super.new(Name,parent) 	;
@@ -14,14 +14,14 @@ class Agent extends uvm_agent;
 	
 	//build phase
 	function void build_phase (uvm_phase Phase)							;
-		super.build_phase(Phase)										;
+		super.build_phase(Phase)								;
 		
 		//TLM
-		my_analysis_port=new("my_analysis_port",this)					;
+		my_analysis_port=new("my_analysis_port",this)						;
 	   
-		Driver1 		= Driver::type_id::create("Driver1",this)		;
-		Monitor1		= Monitor::type_id::create("Monitor1",this)		;
-		Sequencer1		= Sequencer::type_id::create("Sequencer1",this)	;
+		Driver1 		= Driver::type_id::create("Driver1",this)			;
+		Monitor1		= Monitor::type_id::create("Monitor1",this)			;
+		Sequencer1		= Sequencer::type_id::create("Sequencer1",this)			;
 	   
 	   
 		if(!(uvm_config_db #(virtual intf)::get(this,"","vif",Confg_intf)))
@@ -33,15 +33,15 @@ class Agent extends uvm_agent;
 		uvm_config_db #(virtual intf)::set(this,"Driver1","vif",Local_intf) 	;
 		uvm_config_db #(virtual intf)::set(this,"Monitor1","vif",Local_intf)	;
 		
-		$display("agent_build_phase-->done") 									;
+		$display("agent_build_phase-->done") 					;
 	endfunction		
 	
 	//connect phase 
 	function  void connect_phase (uvm_phase Phase)					;
-		super.connect_phase(Phase)									;
+		super.connect_phase(Phase)						;
 		Monitor1.my_analysis_port.connect(my_analysis_port)			;
-		Driver1.seq_item_port.connect(Sequencer1.seq_item_export)	; 
-		$display("agent_connect_phase-->done")						;
+		Driver1.seq_item_port.connect(Sequencer1.seq_item_export)		; 
+		$display("agent_connect_phase-->done")					;
 	endfunction
 
 	
