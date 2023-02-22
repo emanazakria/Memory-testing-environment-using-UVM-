52e6652b13a0aa91f7c57cc9cf4320e7e79c59cd
eman zakria <82594179+emanazakria@users.noreply.github.com>
2023-02-22 22:40:14 +0200

    Update Driver.sv

--- Driver.sv
+++ Driver.sv
@@ -2,15 +2,15 @@ class Driver extends uvm_driver #(Sequence_Item);
 
 	`uvm_object_utils(Driver)
 	
-	function new ( string Name = "Driver" , uvm_component parent = null );
-		super.new(Name,parent) 			;
+	function new ( string Name = "Driver" , uvm_component parent = null )	;
+		super.new(Name,parent) 						;
 	endfunction 
 	
-	virtual intf Driver_intf 			;
-	Sequence_Item Sequenc_Item_Driver 	;
+	virtual intf 	Driver_intf 			;
+	Sequence_Item 	Sequenc_Item_Driver 		;
 	
 	//build phase 
-	function void build_phase (uvm_phase Phase) ;
+	function void build_phase (uvm_phase Phase) 	;
 		super.build_phase(Phase) 		;
 		
 		if(!(uvm_config_db #(virtual intf)::get(this,"","vif",Driver_intf)))
@@ -20,27 +20,27 @@ class Driver extends uvm_driver #(Sequence_Item);
 	endfunction
 		
 	//connect phase 
-	function void connect_phase (uvm_phase Phase) 	;
-		super.connect_phase(Phase) 					;
+	function void connect_phase (uvm_phase Phase) 		;
+		super.connect_phase(Phase) 			;
 		$display("driver_connect_phase-->done")		;
 	endfunction
 	
 	//run phase 
 	task run_phase (uvm_phase Phase);
-		super.run_phase (Phase) 	; 
+		super.run_phase (Phase) ; 
 		
 		forever begin	
-			seq_item_port.get_next_item(Sequence_Item_Driver) 	;
+			seq_item_port.get_next_item(Sequence_Item_Driver) ;
 			
 			@(posedge Driver_intf.clk )
 				Driver_intf.rst  		<= Sequence_Item_Driver.rst 		;
-				Driver_intf.en  		<= Sequence_Item_Driver.en 			;
-				Driver_intf.wr  		<= Sequence_Item_Driver.wr 			;
+				Driver_intf.en  		<= Sequence_Item_Driver.en 		;
+				Driver_intf.wr  		<= Sequence_Item_Driver.wr 		;
 				Driver_intf.Addr  		<= Sequence_Item_Driver.Addr 		;
-				Driver_intf.Data_In  	<= Sequence_Item_Driver.Data_In 	;
+				Driver_intf.Data_In  		<= Sequence_Item_Driver.Data_In 	;
 				
-				$display("%p",Sequence_Item_Driver) 						;
-				seq_item_port.item_done() 									;
+				$display("%p",Sequence_Item_Driver) 					;
+				seq_item_port.item_done() 						;
 		end
 		
 	$display("driver_run_phase-->done");
